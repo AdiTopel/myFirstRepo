d4d9b1c28c1382a8ab5febca7eaa7ab6bc3f86a6
AdiTopel <aditberg@gmail.com>
2022-04-30 17:03:59 +0300

    first commit

---
+++ array.js
@@ -0,0 +1,17 @@
+console.log("hi")
+
+let arr=[15,32,45]
+
+console.log(arr)
+console.log(arr[2])
+
+let fruits=["apple", "banana", "orange"];
+
+
+for (let i=0; i<fruits.length; i++){
+    if (fruits[i] == "banana"){
+        console.log("we have bananas");
+        break
+    }
+    console.log(fruits[i])
+}
